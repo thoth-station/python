4d17416173d42e0be2a018cca944d761cee83d81
Christoph Görn <goern@redhat.com>
2019-11-02 15:37:53 +0100

    bounced version
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@ from setuptools.command.test import test as TestCommand
 
 
 def get_requirements():
-    with open('requirements.txt') as fd:
+    with open("requirements.txt") as fd:
         return fd.read().splitlines()
 
 
 def get_version():
-    with open(os.path.join('thoth', 'python', '__init__.py')) as f:
+    with open(os.path.join("thoth", "python", "__init__.py")) as f:
         content = f.readlines()
 
     for line in content:
-        if line.startswith('__version__ ='):
+        if line.startswith("__version__ ="):
             # dirty, remove trailing and leading chars
-            return line.split(' = ')[1][1:-2]
+            return line.split(" = ")[1][1:-2]
 
     raise ValueError("No package version found")
 
@@ -28,20 +28,18 @@ class Test(TestCommand):
     """Introduce test command to run testsuite using pytest."""
 
     _IMPLICIT_PYTEST_ARGS = [
-        'tests/',
-        '--timeout=2',
-        '--cov=./thoth',
-        '--mypy',
-        '--capture=no',
-        '--verbose',
-        '-l',
-        '-s',
-        '-vv',
+        "tests/",
+        "--timeout=2",
+        "--cov=./thoth",
+        "--mypy",
+        "--capture=no",
+        "--verbose",
+        "-l",
+        "-s",
+        "-vv",
     ]
 
-    user_options = [
-        ('pytest-args=', 'a', "Arguments to pass into py.test")
-    ]
+    user_options = [("pytest-args=", "a", "Arguments to pass into py.test")]
 
     def initialize_options(self):
         super().initialize_options()
@@ -54,6 +52,7 @@ class Test(TestCommand):
 
     def run_tests(self):
         import pytest
+
         passed_args = list(self._IMPLICIT_PYTEST_ARGS)
 
         if self.pytest_args:
@@ -66,21 +65,19 @@ class Test(TestCommand):
 VERSION = get_version()
 
 setup(
-    name='thoth-python',
+    name="thoth-python",
     version=VERSION,
-    packages=[
-        'thoth.python'
-    ],
+    packages=["thoth.python"],
     install_requires=get_requirements(),
-    author='Fridolin Pokorny',
-    author_email='fridolin@redhat.com',
-    maintainer='Fridolin Pokorny',
-    maintainer_email='fridolin@redhat.com',
-    description='A Python ecosystem specific library',
-    long_description=Path('README.rst').read_text(),
-    url='https://github.com/thoth-station/python',
-    license='GPLv3+',
-    keywords='python dependency pypi dependencies tool library thoth',
+    author="Fridolin Pokorny",
+    author_email="fridolin@redhat.com",
+    maintainer="Fridolin Pokorny",
+    maintainer_email="fridolin@redhat.com",
+    description="A Python ecosystem specific library",
+    long_description=Path("README.rst").read_text(),
+    url="https://github.com/thoth-station/python",
+    license="GPLv3+",
+    keywords="python dependency pypi dependencies tool library thoth",
     zip_safe=False,
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -92,12 +89,8 @@ setup(
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
     ],
-    cmdclass={'test': Test},
-    command_options={
-        'build_sphinx': {
-            'version': ('setup.py', VERSION),
-            'release': ('setup.py', VERSION),
-        }
-    }
+    cmdclass={"test": Test},
+    command_options={"build_sphinx": {"version": ("setup.py", VERSION), "release": ("setup.py", VERSION),}},
 )
