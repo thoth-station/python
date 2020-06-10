69c2dc7e4b2f1c8f591d722a7c5354a6b650af4e
Kevin Postlethwait <k.postlethwait24@gmail.com>
2020-06-10 11:10:06 -0400

    Add docstrings for Lazy

--- thoth/python/helpers.py
+++ thoth/python/helpers.py
@@ -187,11 +187,21 @@ def parse_requirements(file_path: str) -> Tuple[List[Source], List[PackageVersio
 
     return sources, package_versions
 
+
 class Lazy(object):
+    """Calculates function exactly once then sets it to be and attribute of object.
+
+    Intended to optimize cases in which a class function is called and does not change
+    after repeated calls. Attribute lookup is ~2x as fast as even the simples function
+    calls.
+    """
+
     def __init__(self, calculate_function):
+        """Create lazy function."""
         self._calculate = calculate_function
 
     def __get__(self, obj, _=None):
+        """Call function and set obj.func_name to value."""
         if obj is None:
             return self
         value = self._calculate(obj)
